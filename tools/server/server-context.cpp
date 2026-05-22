ecff8b9411dd3117df21c87977342a574a2ee92b
cailinxi <linxi.cai@spacemit.com>
2026-05-22 10:37:18 +0800

    server: allow slot erase for SMT multimodal requests

--- tools/server/server-context.cpp
+++ tools/server/server-context.cpp
@@ -2199,9 +2199,6 @@ struct server_context_impl {
                 break;
             case SERVER_TASK_TYPE_SLOT_ERASE:
                 {
-                    if (!check_no_mtmd(task.id)) {
-                        break;
-                    }
                     const int     id_slot = task.slot_action.id_slot;
                     server_slot * slot    = get_slot_by_id(id_slot);
                     if (slot == nullptr) {
